5e2c148b5842f8829dce5cae3a12a32ab473e422
unknown <serhiiromanchuk@gmail.com>
2023-05-25 18:05:45 +0300

    Add and set up TabLayout

--- app/src/main/java/com/example/benchmarks/view/MainActivity.kt
+++ app/src/main/java/com/example/benchmarks/view/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.benchmarks.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.example.benchmarks.R
 import com.example.benchmarks.databinding.ActivityMainBinding
 import com.example.benchmarks.view.adapters.ViewPagerAdapter
 import com.example.benchmarks.view.screens.CollectionsFragment
 import com.example.benchmarks.view.screens.MapsFragment
-import kotlinx.coroutines.launch
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -23,7 +23,47 @@ class MainActivity : AppCompatActivity() {
         val fragmentsList = fillFragmentsList()
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle, fragmentsList)
         binding.viewPager.adapter = viewPagerAdapter
+        setUpTabLayout()
+
         setClickListeners()
+    }
+
+    private fun setUpTabLayout() {
+        with(binding) {
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.view.setBackgroundResource(R.drawable.tab_background_selector_left)
+                        tab.text = "Collections"
+                    }
+                    1 -> {
+                        tab.view.setBackgroundResource(R.drawable.tab_background_selector_right)
+                        tab.text = "Maps"
+                    }
+                }
+            }.attach()
+
+            for (i in 0..1) {
+                val textView = LayoutInflater
+                    .from(this@MainActivity)
+                    .inflate(R.layout.tab_title, null)
+                binding.tabLayout.getTabAt(i)?.customView = textView
+            }
+
+//            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//                override fun onTabSelected(tab: TabLayout.Tab?) {
+//                    tab?.view?.setBackgroundResource(R.drawable.tab_background_selector)
+//                }
+//
+//                override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                }
+//
+//                override fun onTabReselected(tab: TabLayout.Tab?) {
+//                }
+//
+//            })
+        }
     }
 
     private fun fillFragmentsList(): List<Fragment> {
