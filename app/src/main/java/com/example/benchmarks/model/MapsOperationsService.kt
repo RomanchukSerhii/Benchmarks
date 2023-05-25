9a90ebbfcefbdd128b53e797857a090d1008d149
unknown <serhiiromanchuk@gmail.com>
2023-05-25 20:25:40 +0300

    Create MapsOperationsService class and init list of operations

--- app/src/main/java/com/example/benchmarks/model/MapsOperationsService.kt
+++ app/src/main/java/com/example/benchmarks/model/MapsOperationsService.kt
@@ -1,5 +1,9 @@
 package com.example.benchmarks.model
 
+import com.example.benchmarks.model.enums.CollectionOperations
+import com.example.benchmarks.model.enums.CollectionsName
+import com.example.benchmarks.model.enums.MapsName
+import com.example.benchmarks.model.enums.MapsOperations
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import java.util.concurrent.CopyOnWriteArrayList
@@ -10,4 +14,19 @@ class MapsOperationsService {
     private var operations = mutableListOf<Operation>()
     private val listeners = mutableSetOf<ListOperationsListener>()
     private val myCoroutineScope = CoroutineScope(Dispatchers.Default)
+
+    init {
+        for (map in MapsName.values()) {
+            for (operation in MapsOperations.values()) {
+                operations.add(
+                    Operation(
+                        operation.operationName,
+                        map.mapsName,
+                        "0",
+                        false
+                    )
+                )
+            }
+        }
+    }
 }
